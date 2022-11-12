f88047158a787e51b1e0bf4e75c8df591a553ef2
Vighnesh Vasu Vats <vasu8479sharma@gmail.com>
2022-11-12 19:56:50 +0530

    Save  the changes

--- src/template/Adminsignup.js
+++ src/template/Adminsignup.js
@@ -6,12 +6,12 @@ export class Adminsignup extends Component {
       <div>
         <Navbar />
         <div style={{color: "green", backgroundColor: "white",opacity: "0.81245", marginLeft: "375px", marginRight: "375px",marginBottom: "50px", marginTop: "17px", borderTop: "5px solid blue", borderRadius:"10px", fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',' Geneva', 'Verdana', 'sans-serif'",paddingBottom: "5px", paddingLeft: "5px", fontWeight: "bold"}}>
-            <p><center>SignUp for Students</center></p>
+            <p><center>SignUp for Admin</center></p>
             <hr />
             <form>
                 <label htmlFor="name">Name : </label><input type="text" id="name" name="name" style={{marginLeft: "84px"}} />
                 <hr />
-                <label htmlFor="reg_no">Registration No : </label><input type="text" id="reg_no" name="reg_no" style={{marginLeft: "10px"}} />
+                <label htmlFor="reg_no">Employee Id : </label><input type="text" id="reg_no" name="reg_no" style={{marginLeft: "10px"}} />
                 <hr />
                 <label htmlFor="mail">Mail Id : </label><input type="text" id="mail" name="mail" style={{marginLeft: "78px"}} />
                 <hr />
