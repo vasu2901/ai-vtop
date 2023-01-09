35a0fb2f754571d5e1ee2b665e2abb74e45f9084
Vighnesh Vasu Vats <vasu8479sharma@gmail.com>
2023-01-09 20:54:55 +0530

    Save the changes

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const fetchUser = require('../middleware/fetchUser');
 const Marks = require('../Models/Marks');
+const User = require('../models/User');
 const router = express.Router();
+//const { body, validationResult } = require('express-validator');
 router.get('/', fetchUser, async (req, res) => {
-    const mark = await Marks.find();
-    res.json(mark);
+    try {
+        try {
+            if (req.body.reg_no) {
+                const user = await User.findOne({ reg_no: req.body.reg_no });
+                const marks = await Marks.find({ stud : user.id });
+                if (marks) {
+                    return res.json(marks);
+                }
+            }
+            if(req.body.faculty_no)
+            {
+                const marks = await Marks.find({ facultyid : req.body.faculty_no});
+                if (marks) {
+                    return res.json(marks);
+                }
+            }
+            else{
+                const marks = await Marks.find();
+                if (marks) {
+                    return res.json(marks);
+                }
+            }
+        }
+        catch (err) {
+            return res.status(400).send({ message: "No details found" });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(400).send({ message: "Invalid credentials" });
+    }
 })
 
 
